0e71b2705a9bbf38bbb0710319a6fa5e5d326b7c
System <none>
2017-08-02 16:23:55 +0000

    Checkpoint

--- redirect.js
+++ redirect.js
@@ -7,16 +7,15 @@ var redirect = function(){
   MongoClient.connect(url, function(err, db){
     if(err){
       console.log('Unable to connect to Database');
-      db.close();
+      return err;
     } else{
-      var target = '';
       console.log('working');
-      db.collection('testCollection').find({_id: "HJG81OyD-"}).toArray(function(err, data){
-                                                        if(err){console.log('broken')}
-                                                          target = data[url];
-                                                        });
-      db.close();
+      var target = db.collection('testCollection').find({_id: "HJG81OyD-"}, {_id: 0, url: 1}).toString(function(err, data){
+        if(err){return err}
+        console.log(data);
+      });
     }
+    db.close();
   });
 }
 
