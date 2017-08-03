96d871f77585140437373a666bd9d09272a0434c
System <none>
2017-08-03 16:41:52 +0000

    Checkpoint

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 //Pull db url and credentials from external config.js file
 const dbUrl = require('./config.js');
 //Pull mongoose model
-const model = require('./models/shorturl.js');
+const dbModel = require('./models/shorturl.js');
 
 
 //Get connected
@@ -17,9 +17,17 @@ app.get('/', (req, res, next) =>{
 });
 
 app.get('/new/:urlToShorten', (req, res, next)=>{
-  
+  const {urlToShorten} = req.params;
+  const validUrlTest =  /[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi;
+  if(validUrlTest.test(urlToShorten) === true){
+    
+  }
 });
 
 app.get('/:shortForRedirect', (req, res, next)=>{
-  
+  res.end('Working');
+});
+
+app.listen(process.env.PORT, ()=>{
+  console.log("Connected on port: " + process.env.PORT);
 });
